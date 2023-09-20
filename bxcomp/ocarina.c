6bae00ce9dcc832061685695ed9d4bd6d6b9caac
dtrimiizz <96874927+dimitrinhopradoz@users.noreply.github.com>
2023-09-20 03:11:34 -0300

    Update ocarina.c

--- bxcomp/ocarina.c
+++ bxcomp/ocarina.c
@@ -2,38 +2,34 @@
 
 int main() {
     int n; scanf("%i\n", &n);
-    
   
-    char num[13];
-    char po[10] = {'0', '1', '2', '3', '4','5','6','7','8','9'};
-    int ca[10] = {0,0,0,0,0,0,0,0,0,0};
+    char num[13]; int ca[10];
+    
+    int j, i, test;
     
     int count = 0;
-    int j, i;
-    int test;
-    for(i = 0; i < n; i++){
+    
+    while(n--){
         scanf("%s\n", num);
+    
         for(j = 0; j<10; j++){
             ca[j] = 0;
         }
+        
         test = 1;
         count = 0;
+        
         for (j = 0; j < 13; j++){
-            for (int k = 0; k < 10; k++){
-                if(num[j] == po[k]) {
-                    ca[k]++;
-                    break;
-                }
-            }   
+            ca[num[j] - '0']++;
         }
+        
         for (j = 0; j < 10; j++){
-
             if(ca[j] % 2 == 1) count++;
-            if (count > 1){
-                test = 0;
-                break;
-            }
         }
+        if (count > 1) test = 0; 
+        
+
+        
         if (test) {
             printf("Na vasta densa floresta de Hyrule, por longo tempo, servi como espirito guardiao\n");
         }
