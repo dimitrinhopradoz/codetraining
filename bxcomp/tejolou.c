b2bd423e04d708d57a6014493e3cdfb40743e03b
dtrimiizz <96874927+dimitrinhopradoz@users.noreply.github.com>
2023-09-22 13:16:50 -0300

    Update tejolou.c

--- bxcomp/tejolou.c
+++ bxcomp/tejolou.c
@@ -1,16 +1,18 @@
 #include<stdio.h>
 int solve(){
     char prt[4][100] = {"Nitrato de Fala coruja297", "O2wlficiNASA", "GTX-Grace 4090", "Paodo-Competech2077"};
-    int i, j, test = 0;
+    int i, j, curtoX, curtoY, tot, prtt;
+    
     char p[10];
     char mat[6][10];
+    char item = 'A';
+    
     int count1 = 0;
     int count2 = 0;
     int count3 = 0;
-    char item = 'A';
     int posX = -1;
     int posY = -1;
-    
+
     int somaX1 = 0;
     int somaY1 = 0;
     
@@ -33,7 +35,6 @@ int solve(){
     }
     
     if (item != 'A') {
-        test = 1;
         for(i = posX+1; i<10; i++){
             if(mat[posY][i] != '0'){
                 somaX1 = somaX1 + (mat[posY][i] - '0');
@@ -44,8 +45,6 @@ int solve(){
                 somaX2 = somaX2 + (mat[posY][i] - '0');
             }
         }
-    }
-    if (item != 'A') {
         for(i = posY+1; i<6; i++){
             if(mat[i][posX] != '0'){
                 somaY1 = somaY1 + (mat[i][posX] - '0');
@@ -57,29 +56,23 @@ int solve(){
             }
         }
     }
-    
-    int curtoX;
-    int curtoY;
-    int tot;
-    int prtt;
-    int totponto = count1+2*count2+3*count3;
-    
-    if (test){
-        if(somaX1<somaX2) curtoX = somaX1;
-        else curtoX = somaX2;
-        if(somaY1<somaY2) curtoY = somaY1;
-        else curtoY = somaY2;
-        if (curtoX < curtoY) tot = curtoX;
-        else tot = curtoY;
-        if (item == 'F') prtt = 0;
-        else if (item == 'O') prtt = 1;
-        else if (item == 'G') prtt = 2;
-        else prtt = 3;
-        printf("Opa, achei o item, %s, esta na posicao %ix%i, terei que fazer %i pontos para conseguir chegar la.\n",prt[prtt], posY, posX, tot);
-     }
     else{
-        printf("Nossa, cheio de predas, consegui %i pontos.\n", totponto);
+        int totponto = count1+2*count2+3*count3;
+        return printf("Nossa, cheio de predas, consegui %i pontos.\n", totponto);
     }
+    
+
+    if(somaX1<somaX2) curtoX = somaX1;
+    else curtoX = somaX2;
+    if(somaY1<somaY2) curtoY = somaY1;
+    else curtoY = somaY2;
+    if (curtoX < curtoY) tot = curtoX;
+    else tot = curtoY;
+    if (item == 'F') prtt = 0;
+    else if (item == 'O') prtt = 1;
+    else if (item == 'G') prtt = 2;
+    else prtt = 3;
+    printf("Opa, achei o item, %s, esta na posicao %ix%i, terei que fazer %i pontos para conseguir chegar la.\n",prt[prtt], posY, posX, tot);
     
 }
 
